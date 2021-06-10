a0bff2f2889a894b2938488beecae89949deefb5
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-10 10:46:38 +0530

    Validate the DOB text field

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,8 +1,11 @@
 package controller;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import util.MaterialUI;
 
 public class StudentFormController {
@@ -17,6 +20,51 @@ public class StudentFormController {
     public Label lblTitle;
 
     public void initialize() {
-        MaterialUI.paintTextFields( txtNIC, txtFullName, txtAddress, txtDOB,  txtContactNumber, txtEmail);
+        MaterialUI.paintTextFields(txtNIC, txtFullName, txtAddress, txtDOB, txtContactNumber, txtEmail);
+
+        txtDOB.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > 10){
+                txtDOB.setText(txtDOB.getText(0,10));
+            }
+        });
+
+        txtContactNumber.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > 11){
+                txtContactNumber.setText(txtDOB.getText(0,11));
+            }
+        });
+    }
+
+    public void txtDOB_OnKeyTyped(KeyEvent keyEvent) {
+
+        if (keyEvent.getCharacter().equals("-") && (txtDOB.getText().length() == 4 || txtDOB.getText().length() == 7)){
+            return;
+        }
+
+        if (!Character.isDigit(keyEvent.getCharacter().charAt(0))){
+            keyEvent.consume();     // This is not going to forward to the Java FX Runtime Env.
+            return;
+        }
+
+        if ((txtDOB.getText().length() == 4 || txtDOB.getText().length() == 7) && (txtDOB.getCaretPosition() == txtDOB.getLength())){
+            txtDOB.appendText("-");
+            txtDOB.positionCaret(txtDOB.getText().length() +1);
+        }
+    }
+
+    public void txtContactNumber_OnKeyTyped(KeyEvent keyEvent) {
+        if (keyEvent.getCharacter().equals("-") ){
+            return;
+        }
+
+        if (!Character.isDigit(keyEvent.getCharacter().charAt(0))){
+            keyEvent.consume();     // This is not going to forward to the Java FX Runtime Env.
+            return;
+        }
+
+        if (txtContactNumber.getText().length() == 3 ){
+            txtContactNumber.appendText("-");
+            txtContactNumber.positionCaret(txtDOB.getText().length() +1);
+        }
     }
 }
