12f3732b3b83c3fc44c572937af66607fd47d50e
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-10 12:21:27 +0530

    Calculate the age

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -4,9 +4,20 @@ import com.jfoenix.controls.JFXButton;
 import javafx.application.Platform;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.InputMethodEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.util.StringConverter;
+import javafx.util.converter.DateTimeStringConverter;
+import javafx.util.converter.FormatStringConverter;
 import util.MaterialUI;
+
+import java.text.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Date;
 
 public class StudentFormController {
 
@@ -18,19 +29,47 @@ public class StudentFormController {
     public TextField txtDOB;
     public JFXButton btnSave;
     public Label lblTitle;
+    public Label lblAge;
 
     public void initialize() {
         MaterialUI.paintTextFields(txtNIC, txtFullName, txtAddress, txtDOB, txtContactNumber, txtEmail);
 
-        txtDOB.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.length() > 10){
-                txtDOB.setText(txtDOB.getText(0,10));
-            }
-        });
+        setMaxLength(txtDOB, 10);
+        setMaxLength(txtContactNumber, 11);
 
-        txtContactNumber.textProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue.length() > 11){
-                txtContactNumber.setText(txtContactNumber.getText(0,11));
+        txtDOB.textProperty().addListener((observable, oldValue, newValue) -> {
+
+            if (txtDOB.getLength() == 10){
+//                try {
+//                    Date dob = parseDate(txtDOB.getText());
+//                    Date today = new Date();
+//
+//                    long diff = today.getTime() - dob.getTime();
+//                    double year = diff / (1000 * 60 * 60 * 24 * 365.0) ;
+//                    System.out.println("Year: " + year);
+
+                    LocalDate dob2 = LocalDate.parse(txtDOB.getText());
+                    Period between = Period.between(dob2, LocalDate.now());
+
+                    lblAge.setText(between.getYears() + " Years and " + between.getMonths() + " Months " + between.getDays() + " Days old");
+
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                }
+            }
+
+        });
+    }
+
+//    private Date parseDate(String input) throws ParseException {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//        return sdf.parse(input);
+//    }
+
+    private void setMaxLength(TextField txt, int maxLength){
+        txt.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.length() > maxLength){
+                txt.setText(txt.getText(0,maxLength));
             }
         });
     }
