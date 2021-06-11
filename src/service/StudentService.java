26b2c895d6ff1bbe25b2a161371800222f7d296e
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-11 10:32:03 +0530

    Update the StudentService

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -2,32 +2,43 @@ package service;
 
 import model.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentService {
 
-    public void saveStudent(Student student){
+    private static List<Student> studentsDB = new ArrayList<>();
 
+    public void saveStudent(Student student){
+        studentsDB.add(student);
     }
 
     public void updateStudent(Student student){
-
+        Student s = findStudent(student.getNic());
+        int index = studentsDB.indexOf(s);
+        studentsDB.set(index, student);
     }
 
-    public void deleteStudent(String studentId){
-
+    public void deleteStudent(String nic){
+        Student student = findStudent(nic);
+        studentsDB.remove(student);
     }
 
     public List<Student> findAllStudents(){
-        return null;
+        return studentsDB;
     }
 
-    public Student findStudent(String studentId){
+    public Student findStudent(String nic){
+        for (Student student : studentsDB) {
+            if (student.getNic().equals(nic)){
+                return student;
+            }
+        }
         return null;
     }
 
     public List<Student> findStudents(String query){
-        return null;
+       return null;
     }
 
 }
