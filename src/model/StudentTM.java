1d0b169d074a7018a7daaa7b7af4d7a08c2576ab
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-11 12:30:51 +0530

    Add StudentTM

--- src/model/StudentTM.java
+++ src/model/StudentTM.java
@@ -6,4 +6,46 @@ public class StudentTM implements Serializable {
     private String nic;
     private String fullName;
     private String address;
+
+    public StudentTM() {
+    }
+
+    public StudentTM(String nic, String fullName, String address) {
+        this.setNic(nic);
+        this.setFullName(fullName);
+        this.setAddress(address);
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentTM{" +
+                "nic='" + nic + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
