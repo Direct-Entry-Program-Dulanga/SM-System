02c085dd091b09b303581c2c318d66d80a8c666c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-06-11 16:20:48 +0530

    Implement the delete login in the UI

--- src/controller/SearchStudentsFormController.java
+++ src/controller/SearchStudentsFormController.java
@@ -5,12 +5,12 @@ import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.util.Callback;
 import model.Student;
@@ -19,6 +19,7 @@ import service.StudentService;
 import util.MaterialUI;
 
 import java.util.List;
+import java.util.Optional;
 
 public class SearchStudentsFormController {
 
@@ -35,17 +36,16 @@ public class SearchStudentsFormController {
         tblResults.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
         TableColumn<StudentTM, HBox> lastCol = (TableColumn<StudentTM, HBox>) tblResults.getColumns().get(3);
 
-        lastCol.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StudentTM, HBox>, ObservableValue<HBox>>() {
-            @Override
-            public ObservableValue<HBox> call(TableColumn.CellDataFeatures<StudentTM, HBox> param) {
-                ImageView imgEdit = new ImageView("/view/assets/Edit.png");
-                ImageView imgTrash = new ImageView("/view/assets/Trash.png");
+        lastCol.setCellValueFactory(param -> {
+            ImageView imgEdit = new ImageView("/view/assets/Edit.png");
+            ImageView imgTrash = new ImageView("/view/assets/Trash.png");
 
-                imgEdit.getStyleClass().add("action-icons");
-                imgTrash.getStyleClass().add("action-icons");
+            imgEdit.getStyleClass().add("action-icons");
+            imgTrash.getStyleClass().add("action-icons");
 
-                return new ReadOnlyObjectWrapper<>(new HBox(10, imgEdit, imgTrash));
-            }
+            imgTrash.setOnMouseClicked(event -> deleteStudent(param.getValue()));
+
+            return new ReadOnlyObjectWrapper<>(new HBox(10, imgEdit, imgTrash));
         });
 
         txtQuery.textProperty().addListener((observable, oldValue, newValue) -> loadAllStudents(newValue));
@@ -53,11 +53,29 @@ public class SearchStudentsFormController {
         loadAllStudents("");
     }
 
+    private void deleteStudent(StudentTM tm){
+        try {
+            Optional<ButtonType> buttonType = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure to delete this student?", ButtonType.YES, ButtonType.NO).showAndWait();
+            if (buttonType.get() == ButtonType.YES) {
+                studentService.deleteStudent(tm.getNic());
+                tblResults.getItems().remove(tm);
+            }
+        }catch (RuntimeException e){
+            new Alert(Alert.AlertType.ERROR, "Failed to delete the item", ButtonType.OK).show();
+        }
+    }
+
     private void loadAllStudents(String query){
         tblResults.getItems().clear();
 
         for (Student student : studentService.findStudents(query)) {
             tblResults.getItems().add(new StudentTM(student.getNic(), student.getFullName(), student.getAddress()));
+        }
+    }
+
+    public void tblResults_OnKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode() == KeyCode.DELETE){
+            deleteStudent(tblResults.getSelectionModel().getSelectedItem());
         }
     }
 }
